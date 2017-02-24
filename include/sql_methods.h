6b4c29375dde3b3d0e479b43a35e4513e2c35e7b
cathalryan9 <cathalryan92@gmail.com>
2017-02-24 14:45:47 +0000

    Add select method for db polling

--- include/sql_methods.h
+++ include/sql_methods.h
@@ -1,3 +1,5 @@
 //sql_methods.h
 
 void sql_update(std::string table, std::string value, std::string where_stmt );
+
+std::string sql_select(std::string table, std::string value, std::string where_stmt);
--- lib/sql_methods.cc
+++ lib/sql_methods.cc
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "../include/bld/sqlite3.h"
+#include "../include/sqlite-autoconf-3170000/sqlite3.h"
 #include <unistd.h>
 #include <string>
 
@@ -11,17 +11,6 @@ using namespace std;
 //#define ENABLE_DB_TRANSACTION
 #define BUFFER_SIZE 256
 
-//void
-//update_callback ( void *user_data, int operation_type,
-//char const *database, char const *table, sqlite3_int64 rowid)
-//{
-//    //printf("bahhhh\n");
-//    printf ("\n Operation type – %d \n Database – %s\n Table – %s\n  User-Data – %s \n",
-//    operation_type, database, table, (char *)user_data);
-//
-//
-//}
-////
 
 sqlite3 *dbc = NULL;
 sqlite3_stmt *stmt     = NULL;
@@ -30,6 +19,27 @@ std::string sql = "";
 char buf[BUFFER_SIZE] = {0};
 char *path_to_db = "../../rave.db";
 
+struct states{
+    std::string name;
+    std::string value;
+    }state;
+
+
+static int callback(void *unused, int count, char **data, char **columns)
+{
+    int idx;
+
+    printf("There are %d column(s)\n", count);
+
+    for (idx = 0; idx < count; idx++) {
+        printf("The data in column \"%s\" is: %s\n", columns[idx], data[idx]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+////
 void sql_update(std::string table, std::string value, std::string where_stmt ){
 
     /* ===== Connect to DB ===== */
@@ -50,6 +60,28 @@ void sql_update(std::string table, std::string value, std::string where_stmt ){
 
 }
     }
+
+std::string sql_select(std::string table, std::string value, std::string where_stmt ){
+    printf("About to connect\n");
+    int ret = sqlite3_open_v2 (path_to_db, &dbc,
+    SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE,
+    NULL);
+    printf("Opened DB\n");
+    sql = "SELECT " + value + " FROM " + table + " WHERE " + where_stmt +";\n" ;
+    printf(sql.c_str());
+
+    sqlite3_exec(dbc, sql.c_str(), callback, NULL, NULL);
+
+    if (dbc)
+    {
+        sqlite3_close_v2 (dbc);
+        printf("Closed DB\n");
+
+    }
+    //return state.value
+    return "returned";
+
+}
 
 
 
